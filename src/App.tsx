b3717ae72b36e5bdc33fac0a5d3c48dae03fde19
Joseph Surla <josephsurla@gmail.com>
2021-05-04 12:22:34 +0900

    update title and metadata

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import parfait from './79730.jpg';
 function App() {
   return (
     <div className="font-serif h-screen bg-primary">
+      
       <header>
         <h1 className="font-bold text-center mx-auto text-gray-50 text-4xl p-28">&ICECREAM</h1>
-        <img className="object.contain h-80 mx-auto"src={parfait} alt='parfait'/>;
+        <img className="object.contain h-80 mx-auto rounded-xl"src={parfait} alt='parfait'/>;
       </header>
     </div>
   );
